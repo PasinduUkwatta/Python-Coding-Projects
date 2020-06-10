588adbccf0d57f6bfad5268ae3f895293493eb72
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-06-10 10:57:10 +0530

    Practice -Day 7

---
+++ oop.py
@@ -0,0 +1,22 @@
+print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type('hi'))
+print(type([].))
+print(type({}))
+print(type(()))
+
+#create the class
+class BigObject:
+    pass
+
+#instanciate
+object = BigObject()
+
+print(type(BigObject))
+print(type(object))
+
+
+
+
