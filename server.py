aad323d5cd6cb47395bc919c6cd621aa7004c33b
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-07-21 18:33:32 +0530

    User Profile

--- server.py
+++ server.py
@@ -1,18 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect
 app = Flask(__name__)
+print(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!, Pasindu Python Flask Programming'
+def my_home():
+    return render_template('index.html')
 
-hello_world()
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
-#py -m venv env
-#env/Scripts/activate
-#pip install flask
-#set Flask_app={server.py}this is the python file name
-#flask run
-#set FLASK_ENV=development
-#flask run
 
+def write_to_file(data):
+	with open('database.txt',mode='a') as database:
+		email = data["email"]
+		subject =data['subject']
+		message =data["message"]
+
+		file = database.write(f'\n{email},{subject},{message}')
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method =='POST':
+    	data= request.form.to_dict()
+    	write_to_file(data)
+    	return redirect('/thankyou.html')
+    else:
+    	return "Something Went Wrong , Try Again"
