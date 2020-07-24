efe15825b8ff8600e8861ddb7966c060ae4e4dae
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-07-24 09:20:52 +0530

    Practice-Day 23 - Selenium

---
+++ testing_automation.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+chrome_browser = webdriver.Chrome('C:/Users/Pasindu Thiwanka/Downloads/chromedriver.exe')
+
+print(chrome_browser)
+
+
+chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+
+assert 'Selenium Easy Demo' in chrome_browser.title
+
+show_message_button =chrome_browser.find_elements_by_class_name("btn-default")
+
+
+
+assert 'Show Message' in chrome_browser.page_source
+
+user_message =chrome_browser.find_element_by_id('user-message')
+user_button2 =chrome_browser.find_element_by_css_selector('#get-input>.btn')
+print(user_button2)
+user_message.clear()
+user_message.send_keys('Im extra cool')
+
+#show_message_button.click()
+output_message = chrome_browser.find_element_by_id('display')
+
+#assert 'Im extra cool' in output_message.text
