9aeb74146d27a18f2390bf31854df7c2462ca23e
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-08-15 11:59:18 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -1,8 +1,76 @@
-from flask import Flask
-import time
+from flask import Flask,jsonify,request
 
-app= Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-	return {'time':time.time()}
+user_sign_in_details=[
+{
+	'email':'abc@gmail.com',
+	'password':'1234'
+}
+
+]
+
+user_sign_up_details=[
+{
+	'firstname':'abc',
+	'lastname':'def',
+	'email':'abc@gmail.com',
+	'password':'1234',
+}
+
+]
+
+
+@app.route('/',methods ={'POST'})
+def home():
+	request_data =request.get_json()
+	sign_in_details={
+	'email':request_data['email'],
+	'password':request_data['password']
+
+	}
+
+	user_sign_in_details.append(sign_in_details)
+	return jsonify(sign_in_details)
+
+@app.route('/')
+def get_sign_in_details_home():
+    return jsonify({'user_sign_in_details': user_sign_in_details})	
+
+@app.route('/sign-in',methods={'POST'})
+def sign_in():
+	request_data =request.get_json()
+	sign_in_details={
+	'email':request_data['email'],
+	'password':request_data['password']
+
+	}
+
+	user_sign_in_details.append(sign_in_details)
+	return jsonify(sign_in_details)
+
+@app.route('/sign-in')
+def get_sign_in_details():
+    return jsonify({'user_sign_in_details': user_sign_in_details})	
+
+@app.route('/sign-up',methods={'POST'})
+def sign_up():
+	request_data =request.get_json()
+	sign_up_details={
+	'firstname':request_data['firstname'],
+	'lastname':request_data['lastname'],
+	'email':request_data['email'],
+	'password':request_data['password']
+
+	}
+
+	user_sign_up_details.append(sign_up_details)
+	return jsonify(sign_up_details)
+
+
+@app.route('/sign-up')
+def get_sign_up_details():
+    return jsonify({'user_sign_up_details': user_sign_up_details})	
+
+
+app.run(port=5000)
