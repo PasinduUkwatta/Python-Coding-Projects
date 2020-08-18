a98b3d265a6023de4c33906a67c2e1752c565407
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-08-18 15:16:16 +0530

    Rest API

--- app.py
+++ app.py
@@ -1,75 +1,37 @@
-from flask import Flask, jsonify, request,render_template
+from flask import Flask,request
+from flask_restful import Resource,Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate,identity
+
 
 app = Flask(__name__)
+app.secret_key='test'
+api=Api(app)
 
-stores = [
-    {
-        'name': 'my Wonderful Store',
-        'items':
-            [
-                {
-                    'name': 'My Item',
-                    'price': 15
-                }
-            ]
-    }
-]
+jwt = JWT(app,authenticate,identity)
 
+items =[]
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+class Item(Resource):
+    @jwt_required()
+    def get(self,name):
+        item =next(filter(lambda x:x['name']==name,items),None)
+        return{'item':None},200 if item else 404
 
+    def post(self,name):
+        if next(filter(lambda x:x['name']==name,items),None) :
+            return {'message':"An item with name '{}' already exists .".format(name)},400
 
-@app.route('/store', methods=['POST'])
-def create_store():
-    request_data = request.get_json()
-    new_store = {
-        'name': request_data['name'],
-        'items': []
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
+        data=request.get_json()
+        item={'name':name,'price':data['price']}
+        items.append(item)
+        return item,201
 
+class ItemList(Resource):
+    def get(self):
+        return {'items':items}
 
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
+api.add_resource(Item,'/item/<string:name>')
+api.add_resource(ItemList,'/items')
 
-    return jsonify({'message': 'store not found'})
-
-
-@app.route('/store')
-def get_stores():
-    return jsonify({'stores': stores})
-
-
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store['name']==name:
-            new_item={
-                'name':request_data['name'],
-                'price':request_data['price'],
-            }
-            store['items'].append(new_item)
-            return jsonify(new_item)
-
-    return jsonify({'message':'store not found'})
-
-
-
-@app.route('/store/<string:name>/item')
-def get_item_in_store(name):
-    
-    for store in stores:
-        if store['name']==name:
-            return jsonify(store['items'])
-
-    return jsonify({'message':'store is not found'})
-
-
-app.run(port=5000)
+app.run(port=5000,debug=True)
--- user.py
+++ user.py
@@ -1,40 +1,5 @@
 class User:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-
-class Customer(User):
-    def __init__(self, name, age, items):
-        super().__init__(name, age)
-        self.items = items
-
-    def print_details(self):
-        print(f'{self.name} is {self.age} years old &  ordered {self.items}')
-
-
-class Seller(User):
-    def __init__(self, name, age, sells):
-        super().__init__(name, age)
-        self.sells = sells
-
-    def print_details(self):
-        print(f'{self.name} is {self.age} years old &  sells {self.sells}')
-
-
-class Delivery_Person(User):
-    def __init__(self, name, age, delivery):
-        super().__init__(name, age)
-        self.delivery = delivery
-
-    def print_details(self):
-        print(f'{self.name} is {self.age} years old &  deliver {self.delivery}')
-
-
-customer = Customer('Pasindu', 24, 'Flour,Suger,Rice')
-seller = Seller('Saman', 48, 'Milk,Eggs')
-delivery_person = Delivery_Person('John', 32, 'Pizza,Fride Rice')
-
-customer.print_details()
-seller.print_details()
-delivery_person.print_details()
+    def __init__(self,_id,username,password):
+        self.id =id
+        self.username =username
+        self.password =password
