fd94c99f8636a80173a180d0e3a06b392ffc1867
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-02-25 11:58:43 +0530

    Update twitter_access.py

--- twitter_access.py
+++ twitter_access.py
@@ -1,11 +1,11 @@
 import tweepy
 
 
-consumer_key ="IJ3FlcL64zUYm66Xyji3BqSzq"
-consumer_secret="quWqYwWAmmlQTNeYE7Qzk2A1e78TQh5yf3j2gUG5JQmAHIRMCS"
+consumer_key ="xxxx"
+consumer_secret="xxxx"
 
-access_token="926094950176980992-Hs6zAz0hHe0sai3GZNQL7fI7HypTQ6l"
-access_token_secret="5dJIxoHMBHfX4XipnC3JHf0JlT1Vrqbw6cZConU7Fu2Rm"
+access_token="xxxx"
+access_token_secret="xxxx"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
