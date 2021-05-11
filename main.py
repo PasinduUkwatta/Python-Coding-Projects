ea28d591ec4e6e18c612657fed394b8ce6235bda
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-05-11 10:30:22 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+letters =['a','b','z','r','d','f']
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+letters.sort()
 
+numbers=[39,5,93,0,5,91,4,51,15]
+numbers.sort()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(numbers)
