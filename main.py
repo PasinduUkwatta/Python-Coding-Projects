04cc4c1493a7485cf13f188e30bc1cf54bfc9248
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-05-14 18:34:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-letters =['a','b','z','r','d','f']
+test_str = "(7, 8, 9)"
 
-letters.sort()
+# printing original string
+print("The original string is : " + test_str)
+print(type(test_str))
 
-numbers=[39,5,93,0,5,91,4,51,15]
-numbers.sort()
+# Convert Tuple String to Integer Tuple
+# Using eval()
+res = eval(test_str)
 
-print(numbers)
+# printing result
+print("The tuple after conversion is : " + str(res))
+print(type(res))
+
+t = (1, 2, 3)
+print(type(t))
+i = ''.join(str(x) for x in t)
+print(int(i))
+
+print(type(int(i)))
+
+tup = ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's')
+str =  ''.join(tup)
+print(str)
