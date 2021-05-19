9dda92f61ebd8f47051e0cc2fd5758c4e83a9b90
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-05-19 16:56:59 +0530

    convert with loop

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,3 +1,14 @@
 import pandas as pd
-df = pd.read_json (r'C:\Users\Acer\Desktop\New folder\wing_commander_abhinandan_feb14_march_31_split0.json')
-df.to_csv (r'C:\Users\Acer\Desktop\New folder\wing_commander_abhinandan_feb14_march_31_split0.csv', index = None)
+
+for i in range(0,7):
+    word =i
+    print(word)
+
+    json_file =f"wing_commander_abhinandan_feb14_march_31_split{word}.json"
+    csv_file =f"wing_commander_abhinandan_feb14_march_31_split{word}.csv"
+
+
+    df = pd.read_json (fr'C:\Users\Acer\Desktop\New folder\{json_file}')
+    df.to_csv (fr'C:\Users\Acer\Desktop\New folder\{csv_file}', index = None)
+
+    # print(f"r'feb14_march_31_split{i}.json'")
